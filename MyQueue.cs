c4fadaef2c6f8c077655173490f1ea0ada09078d
Bahar Uçar <74768752+baharucar@users.noreply.github.com>
2023-07-18 00:39:50 +0300

    Add files via upload

---
+++ MyQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    internal class MyQueueCls<T>
+    {
+        private int head;
+        private int tail;
+        private int count;
+        private T[] q;
+        private int deFaultQueSize=50;
+
+        public MyQueueCls()
+        {
+            q= new T[deFaultQueSize];
+            count=0;
+            head=0;
+            tail=0;
+        }
+
+        public MyQueueCls(int size)
+        {
+            if (size > 0)
+            {
+                deFaultQueSize = size;
+                q = new T[deFaultQueSize];
+                count = 0;
+                head=0;
+                tail=0;
+            }
+            else
+            {
+                throw new Exception("Size cannot be equal or smaller than zero");
+            }     
+            
+        }
+
+        public void Enqueue(T num) 
+        {
+            if(count >= deFaultQueSize) 
+            {
+                throw new Exception("Queue is full");
+            }
+            else
+            {
+                q[head] = num;
+                count++;
+                head++;
+                head%= deFaultQueSize;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if(count<=0)
+            {
+                throw new Exception("Queue is already empty");
+            }
+            else
+            {
+                T rValue= q[tail];
+                tail++;
+                tail%= deFaultQueSize;
+                count--;
+                return rValue;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using MyQueue;
+
+MyQueueCls<string> obj= new MyQueueCls<string>(5);
+MyQueueCls<int> obj2= new MyQueueCls<int>(5);
+
+obj.Enqueue("Aydin");
+obj.Enqueue("Salih");
+obj.Enqueue("Beyza");
+obj.Enqueue("Melis");
+obj.Enqueue("Onat");
+
+Console.WriteLine(obj.Dequeue());
+Console.WriteLine(obj.Dequeue());
+Console.WriteLine(obj.Dequeue());
+Console.WriteLine(obj.Dequeue());
+Console.WriteLine(obj.Dequeue());
+
+obj2.Enqueue(1);
+obj2.Enqueue(2);
+Console.WriteLine(obj2.Dequeue());
+Console.WriteLine(obj2.Dequeue());
